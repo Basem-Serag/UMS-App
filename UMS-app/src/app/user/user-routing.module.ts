201fda31b6f6b1cbf79ec9b089c686412f19d067
basem <basemserag.911@gmail.com>
2024-05-26 21:49:09 +0300

    bug: fix user routing Edit Profile

--- UMS-app/src/app/user/user-routing.module.ts
+++ UMS-app/src/app/user/user-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
-import { EditProfileComponent } from './components/edit-profile/create-edit-profile.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 
 const routes: Routes = [
   { path: '', component: ProfileComponent },
